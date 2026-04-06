b81b9206910521fdacecd2ae0db26caa21fd83d3
Federico Sosa <FSDevelop@users.noreply.github.com>
2026-04-06 21:51:24 +0000

    online

---
+++ assets/ShipPreviewView-Vi9Mlf4q.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ShipPreview-CUPRSpVs.js";import{q as r,o as a}from"./index-J5NcY_Hs.js";const n={__name:"ShipPreviewView",setup(o){return(t,_)=>(a(),r(e,{standalone:!0}))}};export{n as default};
